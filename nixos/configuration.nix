bece17caf96cf575307ee508430f20edca2724ee
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2025-12-14 19:39:11 +0500

    fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,6 +72,7 @@
   services = {
     desktopManager.plasma6.enable = true;
     displayManager.sddm.enable = true; 
+    displayManager.sddm.wayland.enable = true; 
   };
   # services.flatpak.enable = true;
 
