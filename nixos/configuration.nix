636bd1e914108d4d5dd03e20a1ed61d0d39c3aab
xoliqberdiyev <xoliqberdiyevbehruz12@gmail.com>
2025-12-14 23:03:57 +0500

    fix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -59,7 +59,6 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   networking.networkmanager.enable = true;
-  networking.wireless.enable = false;
 
   time.timeZone = "Asia/Tashkent";
 
